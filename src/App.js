5fa3445fb27ea28a1aad7660715a4139a89bf059
vezsc <hotel300@naver.com>
2024-07-02 15:19:44 +0900

    🥺 마무리 완료

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import HomeView from "./views/HomeView";
+import lenis from "./utils/lenis";
+import link from "./utils/link";
 
 const App = () => {
-    return (
+  useEffect(() => {
+    lenis();
+    link();
+  }, []);
+
+  return (
     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<HomeView />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+      </Routes>
     </BrowserRouter>
-    );
+  );
 };
 
 export default App;
-
